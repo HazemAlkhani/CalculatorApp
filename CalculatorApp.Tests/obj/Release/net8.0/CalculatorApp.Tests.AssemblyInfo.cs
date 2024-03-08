be446ee5eb5e04defc11191239d7b000802cfa9b
HazemAlkhani <hazem007@msn.com>
2024-03-08 10:51:27 +0100

    HH

--- CalculatorApp.Tests/obj/Release/net8.0/CalculatorApp.Tests.AssemblyInfo.cs
+++ CalculatorApp.Tests/obj/Release/net8.0/CalculatorApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6d696cdfa90bc53c7e5392dc995a4211601b5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b06c0f711aa5dbd59c087506d37d073751bb66")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
